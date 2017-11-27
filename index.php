1e1641a5faef4340d0bd9c62019c4f396b3f2980
laptop <creator83@yandex.ru>
2017-11-27 08:31:02 +0300

    first

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Главная страница</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div id="wrapper">
+        <header>
+            <div class="logo">
+                <img src="#" alt="Логотип компании" class="logo__image">
+            </div>
+                <div class="main-menu-list__wrapper main-menu-list__wrapper_about">
+                    <ul class="main-menu-list main-menu-list_about">
+                        <li class="main-menu-list__item">История</li>
+                        <li class="main-menu-list__item">Наши разработки</li>
+                        <li class="main-menu-list__item">Дилеры</li>
+                        <li class="main-menu-list__item">Сертификаты</li>
+                    </ul>
+                </div>
+                <div class="main-menu-list__wrapper main-menu-list__wrapper_clients">
+                    <ul class="main-menu-list main-menu-list_clients">
+                        <li class="main-menu-list__item">Оформление заказа</li>
+                        <li class="main-menu-list__item">Оплата</li>
+                        <li class="main-menu-list__item">Отгрузка и доставка</li>
+                        <li class="main-menu-list__item">Опросные листы</li>
+                        <li class="main-menu-list__item">Конструкторы</li>
+                    </ul>
+                </div>
+            <nav class="main-menu">
+                <div class="main-menu__wrapper main-menu__wrapper_about">
+                    <div class="main-menu__header main-menu__header_about">О предприятии</div>
+                    <div class="arrow-wrapper">
+                        <div class="arrow main-menu__arrow_about">&gt;</div>
+                    </div>
+                </div>
+                <div class="main-menu__wrapper main-menu__wrapper_our-works">
+                    <div class="main-menu__header main-menu__header_our-works">Наши работы</div>
+                    <div class="arrow-wrapper">
+                        <div class="arrow main-menu__arrow_our-works">&gt;</div>
+                    </div>
+                </div>
+                <div class="main-menu__wrapper main-menu__wrapper_price">
+                    <div class="main-menu__header main-menu__header_price">Прайс-лист</div>
+                    <div class="arrow-wrapper">
+                        <div class="arrow main-menu__arrow_price">&gt;</div>
+                    </div>
+                </div>
+                <div class="main-menu__wrapper main-menu__wrapper_client">
+                    <div class="main-menu__header main-menu__header_client">Клиентам</div>
+                    <div class="arrow-wrapper">
+                        <div class="arrow main-menu__arrow_client">&gt;</div>
+                    </div>
+                </div>
+                <div class="main-menu__wrapper main-menu__wrapper_contacts">
+                    <div class="main-menu__header main-menu__header_contacts">Контакты</div>
+                    <div class="arrow-wrapper">
+                        <div class="arrow main-menu__arrow_contacts">&gt;</div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <div class="search-and-status">
+            <div class="search">
+                <input type="text" class="search__text">
+                <div class="search__button">
+                </div>
+            </div>
+            <div class="current-location">
+
+            </div>
+        </div>
+        <div class="main-part">
+            <nav class="catalog">
+                <div class="catalog__header">Каталог</div>
+                <div class="catalog__category catalog__category_filters">
+                    <div class="catalog__category_filters__header">Фильтры</div> 
+                    <div class="catalog__category_filters__list-wrapper">
+                        <ul class="catalog__category_filters__list">
+                        </ul>
+                    </div>
+                </div>
+                <div class="catalog__category catalog__category_deaerators">
+                    <div class="catalog__category_deaerators__header">Деаэраторы</div> 
+                    <div class="catalog__category_deaerators__list-wrapper">
+                        <ul class="catalog__category_deaerators__list">
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="catalog__category catalog__category_separators">
+                    <div class="catalog__category_separators__header">Сепараторы</div> 
+                    <div class="catalog__category_separators__list-wrapper">
+                        <ul class="catalog__category_separators__list">
+                        </ul>
+                    </div>    
+                </div>
+                <div class="catalog__category catalog__category_mud-filters">
+                    <div class="catalog__category_mud-filters__header">Грязевые фильтры</div> 
+                    <div class="catalog__category_mud-filters__list-wrapper">
+                        <ul class="catalog__category_mud-filters__list">
+                        </ul>
+                    </div>    
+                </div>
+                <div class="catalog__category catalog__category_sludge-rejectors">
+                    <div class="catalog__category_sludge-rejectors__header">Шламоотводители</div> 
+                    <div class="catalog__category_sludge-rejectors__list-wrapper">
+                        <ul class="catalog__category_sludge-rejectors__list">
+                        </ul>
+                    </div>    
+                </div>
+                <div class="catalog__category catalog__category_air-collectors">
+                    <div class="catalog__category_air-collectors__header">Воздухосборники</div> 
+                    <div class="catalog__category_air-collectors__list-wrapper">
+                        <ul class="catalog__category_air-collectors__list">
+                        </ul>
+                    </div>    
+                </div>
+                <div class="catalog__category catalog__category_engineering">
+                    <div class="catalog__category_engineering__header">Инженерное</div> 
+                    <div class="catalog__category_engineering__list-wrapper">
+                        <ul class="catalog__category_engineering__list">
+                        </ul>
+                    </div>    
+                </div>
+                <div class="catalog__category catalog__category_magnetic-systems">
+                    <div class="catalog__category_magnetic-systems__header">Магнитные системы</div> 
+                    <div class="catalog__category_magnetic-systems__list-wrapper">
+                        <ul class="catalog__category_magnetic-systems__list">
+                        </ul>
+                    </div>    
+                </div>
+                <div class="catalog__category catalog__category_heating">
+                    <div class="catalog__category_heating__header">Отопительное</div> 
+                    <div class="catalog__category_heating__list-wrapper">
+                        <ul class="catalog__category_heating__list">
+                        </ul>
+                    </div>    
+                </div>
+            </nav>
+            <div class="main-part__wrapper">
+                
+            </div>
+        </div>
+        <footer>
+        </footer>
+    </div>
+</body>
+</html>
